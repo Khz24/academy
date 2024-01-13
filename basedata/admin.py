4a0f3f7eba0a7a227fa7e358b17bf8acf5cffe3b
admin <Khalil.Zeiaei@Gmail.Com>
2024-01-13 18:32:12 +0330

    add filter and search

--- basedata/admin.py
+++ basedata/admin.py
@@ -5,6 +5,10 @@ from .models import Student, Level, Course, StudentCourse, StudentProfile
 # Register your models here.
 class StudentAdmin(admin.ModelAdmin):
     list_display = ('student_id', 'name', 'family', 'status', 'sex',)
+    search_fields = ('student_id', )
+    list_filter = ('sex', 'status',)
+    ordering = ('student_id',)
+    list_per_page =10
 
 
 admin.site.register(Student, StudentAdmin)
