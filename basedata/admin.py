fb3a860bc8dd0995d72ac8632c96bd1cc10e4185
admin <Khalil.Zeiaei@Gmail.Com>
2024-01-13 18:12:42 +0330

    add custom admin panel

--- basedata/admin.py
+++ basedata/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Student, Level, Course, StudentCourse, StudentProfile
+
 
 # Register your models here.
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('student_id', 'name', 'family', 'status', 'sex',)
+
+
+admin.site.register(Student, StudentAdmin)
+admin.site.register(Level)
+admin.site.register(Course)
+admin.site.register(StudentCourse)
+admin.site.register(StudentProfile)
